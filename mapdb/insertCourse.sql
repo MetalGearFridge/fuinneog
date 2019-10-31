0caaeb7418f6322328eb372328323475278129ab
ryanjoness <43958299+ryanjoness@users.noreply.github.com>
2019-10-31 14:23:37 +0000

    Update insertCourse.sql

--- mapdb/insertCourse.sql
+++ mapdb/insertCourse.sql
@@ -1,5 +1,25 @@
-INSERT INTO Course (cName, cLocation, cDate, cDescription,MaxAttend )
+INSERT INTO Course (cName, cLocation, cDate, cDescription, MaxAttend )
 VALUES
-(Professionalism, Belfast, 01-11-19,
-This course ventures into the depths of professionalism within a company and gives the trainees insight in a company, 20 )
+('Professionalism', 'Belfast', 2019-11-01,
+'This course ventures into the depths of professionalism within a company and gives the trainees insight in a company', 20 ),
+
+('Managing Your Career', 'London', 2019-12-02,
+'This course ventures into the depths of professionalism within a company and gives the trainees insight in a company', 30 ),
+
+('PLC Compliance', 'Dublin', 2019-11-03,
+'This course ventures into the depths of professionalism within a company and gives the trainees insight in a company', 50 ),
+
+('Unconscious Bias', 'Belfast', 2019-12-04,
+'This course ventures into the depths of professionalism within a company and gives the trainees insight in a company', 40 ),
+
+('Corporate Data safeguarding', 'Atlanta', 2019-11-05,
+'This course ventures into the depths of professionalism within a company and gives the trainees insight in a company', 5 ),
+
+('Security Data', 'Birmingham', 2019-12-06,
+'This course ventures into the depths of professionalism within a company and gives the trainees insight in a company', 7 ),
+
+('Despacito 2', 'London', 2019-11-07,
+'This course ventures into the depths of professionalism within a company and gives the trainees insight in a company', 14 ),
+
+
 ;
