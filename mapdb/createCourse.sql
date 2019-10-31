04dff486507ce964d8e250028e6736e256301e94
MetalGearFridge <43957657+MetalGearFridge@users.noreply.github.com>
2019-10-31 14:05:22 +0000

    Made some misc fixes

--- mapdb/createCourse.sql
+++ mapdb/createCourse.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Course (
     cid int PRIMARY KEY AUTO_INCREMENT,
-    cName VARCHAR(100) NOTNULL,
+    cName VARCHAR(100) NOT NULL,
     cLocation VARCHAR(100),
-    cDate DATETIME NOT NULL DEFAULT(GETDATE()),
+    cDate DATETIME NOT NULL,
     cDescription VARCHAR(510),
     tid int NOT NULL,
     MaxAttend int,
