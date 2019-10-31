30e9d8fba397213a358f1d2ec9b676a5ceddd0bc
MetalGearFridge <43957657+MetalGearFridge@users.noreply.github.com>
2019-10-31 14:07:02 +0000

    changed format

--- mapdb/createEmployee.sql
+++ mapdb/createEmployee.sql
@@ -2,8 +2,8 @@ CREATE DATABASE mapdb;
 
 CREATE TABLE Employee(
     eid INT PRIMARY KEY AUTO_INCREMENT,
-    empName VARCHAR(50) NOT NULL,
-    empEmail VARCHAR(50) NOT NULL,
+    eName VARCHAR(50) NOT NULL,
+    eEmail VARCHAR(50) NOT NULL,
     officeLocation VARCHAR(50) NOT NULL,
     managerEmail VARCHAR(50) NOT NULL
     );
