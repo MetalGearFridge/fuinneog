e0ca692ec2c31b4bf144d75c848a9733f28a3662
amyasmith <amys@kainos.com>
2019-10-31 14:32:22 +0000

    createdatabase script

--- mapdb/createWaitingList.sql
+++ mapdb/createWaitingList.sql
@@ -1,8 +1,8 @@
 CREATE TABLE WaitingList (
-    eid int FOREIGN KEY NOT NULL,
-    cid int FOREIGN KEY NOT NULL,
-    TimeStamp datetime NOT NULL DEFAULT(GETDATE()),
-    PRIMARY KEY(eid, eid),
+    eid int NOT NULL,
+    cid int NOT NULL,
+    TimeStamp datetime NOT NULL DEFAULT(CURDATE()),
+    PRIMARY KEY(eid, cid),
     FOREIGN KEY (eid) REFERENCES Employee(eid),
     FOREIGN KEY (eid) REFERENCES Course(cid)
 );
--- mapdb/insertEmployee.sql
+++ mapdb/insertEmployee.sql
@@ -1,11 +1,11 @@
 INSERT INTO Employee (empname,emplEmail,officeLocation,managerEmail)
-VALUES (Daniel,DamnDaniel@kainos.com,London,T.Tok@kainos.com); 
+VALUES ('Daniel','DamnDaniel@kainos.com','London','T.Tok@kainos.com'); 
 INSERT INTO Employee (empname,emplEmail,officeLocation,managerEmail)
-VALUES (Peterson,P.Percy@kainos.com,London,T.Tok@kainos.com); 
+VALUES ('Peterson','P.Percy@kainos.com','London','T.Tok@kainos.com'); 
 INSERT INTO Employee (empname,emplEmail,officeLocation,managerEmail)
-VALUES (Broisin,B.Badman@kainos.com,Belfast,J.Mairs@kainos.com); 
+VALUES ('Broisin','B.Badman@kainos.com','Belfast','J.Mairs@kainos.com'); 
 INSERT INTO Employee (empname,emplEmail,officeLocation,managerEmail)
-VALUES (Alice,A.Alice@kainos.com,Gdansk,P.Gzwarkszkf@kainos.com); 
+VALUES ('Alice','A.Alice@kainos.com','Gdansk','P.Gzwarkszkf@kainos.com'); 
 INSERT INTO Employee (empname,emplEmail,officeLocation,managerEmail)
 VALUES (Luke,L.Short@kainos.com,London,T.all@kainos.com); 
 INSERT INTO Employee (empname,emplEmail,officeLocation,managerEmail)
