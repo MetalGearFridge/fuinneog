9e794a125982a967d687d2d8952dba1175ccb5f8
ryanjoness <43958299+ryanjoness@users.noreply.github.com>
2019-10-31 14:04:21 +0000

    Add files via upload

---
+++ mapdb/insertCourse.sql
@@ -0,0 +1,5 @@
+INSERT INTO Course (cName, cLocation, cDate, cDescription,MaxAttend )
+VALUES
+(Professionalism, Belfast, 01-11-19,
+This course ventures into the depths of professionalism within a company and gives the trainees insight in a company, 20 )
+;
