5f70dd9d2d4e357f148c2dc06deccc5825f0324e
Luke M <lukemcilwaine@outlook.com>
2019-11-01 11:34:26 +0000

    Updates to UI

--- window/src/app/app.component.ts
+++ window/src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'window';
+  
+  courses = [{
+    id: 100,
+    name: "How to use Git",
+    date: "01/10/2019",
+    location: "Park Inn"
+  },
+  {
+    id: 100,
+    name: "Use Git Properly",
+    date: "02/10/2019",
+    location: "Park Inn"
+  },
+  {
+    id: 100,
+    name: "Just can't Git this",
+    date: "03/10/2019",
+    location: "Park Inn"
+  },
+  {
+    id: 100,
+    name: "Na Na Na Na",
+    date: "20/10/2019",
+    location: "Park Inn"
+  },
+]
+
 }
